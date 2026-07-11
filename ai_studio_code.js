f8b8fb996bbd6511a17d7af6448131042e524952
avinashkumarakrd-afk <avinash.kumarakrd@gmail.com>
2026-07-11 11:46:18 +0530

    Add files via upload

---
+++ ai_studio_code.js
@@ -0,0 +1,22 @@
+// A simple service worker to allow installation as a PWA
+const CACHE_NAME = 'neet-system-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
